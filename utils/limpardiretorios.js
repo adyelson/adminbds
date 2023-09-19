10f7877813b629f760015db50c2a490e429dbce8
adyelson <adyelson.loureiro@gmail.com>
2023-09-19 23:55:35 +0000

    restruturado2!

--- utils/limpardiretorios.js
+++ utils/limpardiretorios.js
@@ -1,6 +1,13 @@
 const { exec } = require('child_process');
 function limparDiretorios() {
-  const comandos = ['sudo rm /workspace/adminbds/arquivos/descompactados/*', 'sudo rm/workspace/adminbds/arquivos/json/*', 'sudo rm /workspace/adminbds/arquivos/recebidos/*'];
+  const comandos = [
+  'sudo rm /workspace/adminbds/arquivos/descompactados/*', 
+  'sudo rm/workspace/adminbds/arquivos/json/*', 
+  'sudo rm /workspace/adminbds/arquivos/recebidos/*',
+  'sudo rm /workspace/adminbds/arquivos/preparacaojson/*',
+  'sudo rm /workspace/adminbds/arquivos/txtsalvo/*',
+  'sudo rm /workspace/adminbds/arquivos/arquivosEXP/*',
+];
   comandos.forEach(comando => {
     exec(comando, (erro, stdout, stderr) => {
       if (erro) {
