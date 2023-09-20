795c123d9105e29f237c53034f95a9a990cce5a8
adyelson <adyelson.loureiro@gmail.com>
2023-09-20 22:49:42 +0000

    faz geografico e operacional

--- utils/converterjsonptexto.js
+++ utils/converterjsonptexto.js
@@ -11,32 +11,63 @@ function converterJsonPtexto(nomeDoArquivo) {
         reject(err);
         return;
       }
-      try {
-        const dadosJSON = JSON.parse(content);
-        for (let i = 1; i <= 8; i++) {
-          arquivoTexto += '# \n';
-        }
-        const chaves = Object.keys(dadosJSON[0]).join('\t').toUpperCase();
-        arquivoTexto += `# ${chaves}\n`;
-        for (const jsonData of dadosJSON) {
-          for (const chave in jsonData) {
-            arquivoTexto += jsonData[chave] + '\t'; // Use o delimitador apropriado, como '\t' (tabulação)
+      if (nomeSemExtensao === nomeSemExtensao.toUpperCase()) {
+        try {
+          const dadosJSON = JSON.parse(content);
+          for (let i = 1; i <= 8; i++) {
+            arquivoTexto += '# \n';
           }
-          arquivoTexto += '\n'; // Adicione uma quebra de linha após cada conjunto de dados
-        }
-        // Escreva o arquivo de texto no servidor
-        fs.writeFile(caminhoArquivoTexto, arquivoTexto, 'utf8', (err) => {
-          if (err) {
-            console.error('Erro ao escrever o arquivo de texto:', err);
-            reject(err);
-          } else {
-            console.log('Arquivo de texto salvo com sucesso:', caminhoArquivoTexto);
-            resolve(caminhoArquivoTexto); // Resolve com o caminho do arquivo de texto salvo
+          const chaves = Object.keys(dadosJSON[0]).join('\t').toUpperCase();
+          arquivoTexto += `# ${chaves}\n`;
+          for (const jsonData of dadosJSON) {
+            for (const chave in jsonData) {
+              arquivoTexto += jsonData[chave] + '\t'; // Use o delimitador apropriado, como '\t' (tabulação)
+            }
+            arquivoTexto += '\n'; // Adicione uma quebra de linha após cada conjunto de dados
           }
-        });
-      } catch (error) {
-        console.error('Erro ao fazer parsing do JSON:', error);
-        reject(error);
+          // Escreva o arquivo de texto no servidor
+          fs.writeFile(caminhoArquivoTexto, arquivoTexto, 'utf8', (err) => {
+            if (err) {
+              console.error('Erro ao escrever o arquivo de texto:', err);
+              reject(err);
+            } else {
+              console.log('Arquivo de texto salvo com sucesso:', caminhoArquivoTexto);
+              resolve(caminhoArquivoTexto); // Resolve com o caminho do arquivo de texto salvo
+            }
+          });
+        } catch (error) {
+          console.error('Erro ao fazer parsing do JSON:', error);
+          reject(error);
+        }
+      } else {
+        try {
+          const dadosJSON = JSON.parse(content);
+          arquivoTexto += `****************************************************************\n`;
+          for (let i = 2; i <= 10; i++) {
+            arquivoTexto += '\n';
+          }
+          arquivoTexto += `****************************************************************\n`;
+          arquivoTexto += `----\n`;
+          for (const jsonData of dadosJSON) {
+            for (const chave in jsonData) {
+              arquivoTexto += jsonData[chave] + ';'; // Use o delimitador apropriado, como '\t' (tabulação)
+            }
+            arquivoTexto += '\n'; // Adicione uma quebra de linha após cada conjunto de dados
+          }
+          // Escreva o arquivo de texto no servidor
+          fs.writeFile(caminhoArquivoTexto, arquivoTexto, 'utf8', (err) => {
+            if (err) {
+              console.error('Erro ao escrever o arquivo de texto:', err);
+              reject(err);
+            } else {
+              console.log('Arquivo de texto salvo com sucesso:', caminhoArquivoTexto);
+              resolve(caminhoArquivoTexto); // Resolve com o caminho do arquivo de texto salvo
+            }
+          });
+        } catch (error) {
+          console.error('Erro ao fazer parsing do JSON:', error);
+          reject(error);
+        }
       }
     });
   });
